feb46fc07e5680244b30ee38e382040737f72367
Samuel Yawson <s.yawson@4th-ir.com>
2023-10-31 23:48:39 +0000

    used flair model from flair library for NER

--- extract_ner.py
+++ extract_ner.py
@@ -1,14 +1,8 @@
-from transformers import AutoModelForTokenClassification, AutoTokenizer, pipeline
+from flair.data import Sentence
+from flair.models import SequenceTagger
 
-# Define the model repo
-model_name = "flair/ner-multi-fast"
-
-# Download the model and tokenizer
-model = AutoModelForTokenClassification.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-# Create a NER pipeline
-nlp = pipeline("ner", model=model, tokenizer=tokenizer)
+# Load the pre-trained Flair NER model for English
+tagger = SequenceTagger.load("flair/ner-english-ontonotes-large")
 
 # Create empty lists to store your data
 first_names = []
@@ -17,13 +11,7 @@ addresses = []
 phones = []
 dates_of_birth = []
 
-# Define a function to capture the information from text using the Hugging Face Flair model
-def extract_info(text,
-                 first_names=first_names,
-                 last_names=last_names,
-                 addresses=addresses,
-                 phones=phones,
-                 dates_of_birth=dates_of_birth):
+def extract_info(text):
     # Initialize variables to store extracted information
     first_name = ""
     last_name = ""
@@ -31,42 +19,37 @@ def extract_info(text,
     phone = ""
     date_of_birth = ""
 
-    # Use the Hugging Face Flair NER model for prediction
-    inputs = tokenizer(text, return_tensors="pt")
-    
-    # Apply nodel on tokenized input
-    outputs = model(**inputs)
-    
-    print(outputs)
+    # Create a Flair sentence
+    sentence = Sentence(text)
 
-    # # Extract the information using Named Entity Recognition
-    # for entity in results:
-    #     entity_text = entity['word']
-    #     entity_label = entity['entity']
+    # Run NER on the sentence
+    tagger.predict(sentence)
+
+    for entity in sentence.get_spans('ner'):
+        print(entity)
+        entity_text = entity.text
+        entity_label = entity.tag
+
+        if entity_label == 'DATE':
+            date_of_birth = entity_text.strip()
+
+        elif entity_label == 'LOC' or entity_label == 'GPE':
+            address = entity_text.strip()
+
+        elif entity_label == 'PERSON':
+            if not first_name:
+                first_name = entity_text.strip()
+            else:
+                last_name = entity_text.strip()
+
+        elif entity_label == 'PHONE':
+            phone = entity_text.strip()
         
-    #     if entity_label == 'DATE':
-    #         # DATE entity type for dates, which could include date of birth
-    #         date_of_birth = entity_text.strip()
+        # Append the extracted information to your pre-defined lists as needed
+        first_names.append(first_name)
+        last_names.append(last_name)
+        addresses.append(address)
+        phones.append(phone)
+        dates_of_birth.append(date_of_birth)
 
-    #     elif entity_label == 'LOC':
-    #         # LOC represents location entities, which could include addresses
-    #         address = entity_text.strip()
-
-    #     elif entity_label == 'PER':
-    #         # PER entity type for persons, which could include first and last names
-    #         if first_name:
-    #             # If we already have a first name, assume the current entity is the last name
-    #             last_name = entity_text.strip()
-    #         else:
-    #             first_name = entity_text.strip()
-
-    #     elif entity_label == 'PHONE':
-    #         # PHONE entity type (custom) for phone numbers
-    #         phone = entity_text.strip()
-
-    # # Append the extracted information to the pre-defined lists
-    # first_names.append(first_name)
-    # last_names.append(last_name)
-    # addresses.append(address)
-    # phones.append(phone)
-    # dates_of_birth.append(date_of_birth)
+    return first_names, last_names, addresses, phones, dates_of_birth
