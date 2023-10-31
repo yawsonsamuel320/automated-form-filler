d2c9a51b1e2c504bb3d0b0409a27a5fafbd7248d
Samuel Yawson <s.yawson@4th-ir.com>
2023-10-31 23:51:07 +0000

    included new ner extraction function to text_extraction.py

--- text_extraction.py
+++ text_extraction.py
@@ -4,6 +4,13 @@ from PyPDF2 import PdfReader
 # Import internal project file/functions
 from extract_ner import extract_info
 
+# Create empty lists to store your data
+first_names = []
+last_names = []
+addresses = []
+phones = []
+dates_of_birth = []
+
 # Open PDF File
 pdf_file = PdfReader(open("./data/sample.pdf", "rb"))
 
@@ -13,4 +20,11 @@ pages = [pdf_file.pages[i] for i in range(len(pdf_file.pages))]
 # Join all the pages into a string
 text_from_pdf = "\n".join([page.extract_text() for page in pages])
 
-extract_info(text_from_pdf)
+first_names, last_names, addresses, phones, dates_of_birth = extract_info(text_from_pdf)
+
+
+print(list(set(first_names)))
+print(list(set(last_names)))
+print(list(set(addresses)))
+print(list(set(phones)))
+print(list(set(dates_of_birth)))
