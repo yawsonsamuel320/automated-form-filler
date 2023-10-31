960439c7e8bf415c6bd62b2cd020227b81856e37
Samuel Yawson <s.yawson@4th-ir.com>
2023-10-31 23:52:44 +0000

    tested input for field for new google form

--- webdriver.py
+++ webdriver.py
@@ -1,32 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-# Set the path to your WebDriver executable. You should download the appropriate WebDriver for your browser (e.g., Chrome, Firefox).
+import time
+
 driver = webdriver.Chrome()
+driver.get("https://forms.gle/zJpVE1ck4rxXbWFa7")
 
-# Open the Google Form URL
-form_url = 'https://forms.gle/vWVmojtWdfFvEj8V6'
-driver.get(form_url)
+# Wait for one second, until page gets fully loaded
+time.sleep(1)
 
-# Fill out the form fields
-name = driver.find_elements("class", "ndJi5d snByac")  # Replace "entry.123456789" with the actual field name or ID for Name
-name.send_keys("Your Name")
+inputs = {
+    "First Name": "Samuel",
+    "Last Name": "Yawson",
+    "Phone": 1,
+}
+for input_name in inputs:
+    text_input = driver.find_element(By.XPATH, f"//div[contains(@jsmodel, 'CP1oW') and contains(., '{input_name}')]//input[@type='text']")
+    text_input.send_keys(inputs[input_name])
+    time.sleep(2)
 
-email = driver.find_element_by_name("entry.987654321")  # Replace "entry.987654321" with the actual field name or ID for Email
-email.send_keys("your@email.com")
-
-address = driver.find_element_by_name("entry.567890123")  # Replace "entry.567890123" with the actual field name or ID for Address
-address.send_keys("Your Address")
-
-phone = driver.find_element_by_name("entry.456789012")  # Replace "entry.456789012" with the actual field name or ID for Phone
-phone.send_keys("123-456-7890")
-
-comments = driver.find_element_by_name("entry.345678901")  # Replace "entry.345678901" with the actual field name or ID for Comments
-comments.send_keys("Your comments here")
-
-# Submit the form
-submit_button = driver.find_element_by_xpath("//span[text()='Submit']")  # Replace with the text on the submit button
-submit_button.click()
-
-# Close the browser
-driver.quit()
+driver.close()
